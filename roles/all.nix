e6c5e39542f98ce9e9c3cf04c43847b0c8f18698
fleaz <mail@felixbreidenstein.de>
2022-02-23 23:41:48 +0100

    Added remote builder in secrets repo

--- roles/all.nix
+++ roles/all.nix
@@ -5,7 +5,6 @@
     ../modules/earlyoom.nix
     ../modules/fonts.nix
     ../modules/opengl.nix
-    ../modules/remote-builder.nix
     ../modules/sound.nix
     ../modules/ssh.nix
     ../modules/docker.nix
