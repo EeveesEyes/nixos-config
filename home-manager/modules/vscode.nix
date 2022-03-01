3fee8b0b0909320c9c607362c3397687c0578240
fleaz <mail@felixbreidenstein.de>
2022-03-01 19:58:09 +0100

    vscode: Switch to unfree vscode team

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -2,7 +2,6 @@
 
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium; # You can skip this if you want to use the unfree version
     extensions = with pkgs.vscode-extensions; [
       # Dark Theme
       dracula-theme.theme-dracula
