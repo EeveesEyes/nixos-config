ea298287719396ceae68040dfd901ae9345a6475
fleaz <mail@felixbreidenstein.de>
2022-03-02 09:46:55 +0100

    kanshi: Write config for laptop and homeoffice

--- home-manager/modules/kanshi.nix
+++ home-manager/modules/kanshi.nix
@@ -1,6 +1,35 @@
 {
   services.kanshi = {
     enable = true;
+    profiles = {
+      "laptop" = {
+        outputs = [
+	  {
+	    criteria = "eDP-1";
+	    scale = 1.8;
+	  }
+	];
+      };
+      "homeoffice" = {
+        outputs = [
+	  {
+	    criteria = "eDP-1";
+	    scale = 1.8;
+	    position = "0,0";
+	  }
+	  {
+	    criteria = "DP-3";
+	    scale = 1.3;
+	    position = "2133,0";
+	  }
+	  {
+	    criteria = "DP-1";
+	    scale = 1.3;
+	    position = "5087,0";
+	  }
+	];
+      };
+    };
   };
 
 }
