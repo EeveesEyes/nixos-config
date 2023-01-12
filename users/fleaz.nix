da48387f24f8a11866c6ed91910f386518653650
fleaz <mail@felixbreidenstein.de>
2023-01-12 22:13:45 +0100

    users/fleaz: Fix typo in group name

--- users/fleaz.nix
+++ users/fleaz.nix
@@ -5,7 +5,7 @@
       "wheel" # Enable ‘sudo’ for the user.
       "networkmanager" # Access to networkmanager
       "docker" # Access to the "/run/docker.sock"
-      "dialup" # for serial access
+      "dialout" # for serial access
     ];
     shell = pkgs.zsh;
     hashedPassword = "$6$9dARC6e2RxgPC9f1$QfXpT71cXA7YiFhv75Nnq2OrbQ8xlHMzgrJdaBaETaAVHLX5j8QUAl71dxMlqD.CtTLxe566kL5Q6da7Kqnvp/";
