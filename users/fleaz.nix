13868e69064282f09d59eda52e6c1cddce0237a5
fleaz <mail@felixbreidenstein.de>
2023-06-15 10:57:50 +0200

    users/fleaz: Add all my ssh pubkeys to my user

--- users/fleaz.nix
+++ users/fleaz.nix
@@ -3,13 +3,21 @@
 
   users.users.fleaz = {
     isNormalUser = true;
+    shell = pkgs.zsh;
+    hashedPassword = "$6$9dARC6e2RxgPC9f1$QfXpT71cXA7YiFhv75Nnq2OrbQ8xlHMzgrJdaBaETaAVHLX5j8QUAl71dxMlqD.CtTLxe566kL5Q6da7Kqnvp/";
+
     extraGroups = [
       "wheel" # Enable ‘sudo’ for the user.
       "networkmanager" # Access to networkmanager
       "docker" # Access to the "/run/docker.sock"
       "dialout" # for serial access
     ];
-    shell = pkgs.zsh;
-    hashedPassword = "$6$9dARC6e2RxgPC9f1$QfXpT71cXA7YiFhv75Nnq2OrbQ8xlHMzgrJdaBaETaAVHLX5j8QUAl71dxMlqD.CtTLxe566kL5Q6da7Kqnvp/";
+
+    # Allow ssh between all my machines
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOQB+LpTMWkmrx/ve1gxfzCM3CAsWpYkQ5QBRH1Vqf8 fleaz@cray"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOufg1IAWXQBbUPTc3W3vORxFc94/MbbaYzpimqI+M/J fleaz@jimbo"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpdn6umACgFp2zucdvjHclYVZxUxWNZZvM7/h6HcJ+x fleaz@milhouse"
+    ];
   };
 }
