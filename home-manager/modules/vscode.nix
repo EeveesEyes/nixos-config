d1b6d1de00515c5bac6f248b1ef444aeb4f8cc99
fleaz <mail@felixbreidenstein.de>
2023-07-22 16:32:34 +0200

    modules/vscode: Switch to proprietary build

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -1,27 +1,12 @@
-{ pkgs, ... }:
-let
-  unstable = import <nixos-unstable> { };
-in
+{ pkgs,  ... }:
 {
 
   programs.vscode = {
     enable = true;
-    package = unstable.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      # Dark Theme
-      dracula-theme.theme-dracula
 
-      # Terraform and HCL support
-      hashicorp.terraform
-
-      # Python
-      ms-python.python
-
-      # Vim 
-      vscodevim.vim
-
-      # Markdown highlighting/formatting/preview
-      yzhang.markdown-all-in-one
-    ];
+    # Use the proprietary VSCode build from Microsft
+    # This way we get the "Settings Sync" feature
+    package = pkgs.vscode;
+    extensions = [];
   };
 }
