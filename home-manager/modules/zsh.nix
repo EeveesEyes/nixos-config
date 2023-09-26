861db3fd250bd43c015cc8bf3a1e735310003289
fleaz <mail@felixbreidenstein.de>
2023-09-26 16:21:05 +0200

    modules/nvim: add missing variables and aliases

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -11,9 +11,15 @@
       size = 10000;
     };
     shellAliases = {
+      "k" = "kubectl";
       "dl" = "ls -lhtr --color=always ~/Downloads | tail -n 10"; # Show the 10 newest Downloads
       "buzzer" = "ssh -i Nextcloud/Privat/id_door door@door.w17.io buzzer";
     };
+    initExtra = ''
+      export EDITOR="nvim";
+      export PATH="$PATH:$HOME/.krew/bin:$HOME/bin:$HOME/go/bin";
+      source <(kubectl completion zsh)
+    '';
   };
 
 }
