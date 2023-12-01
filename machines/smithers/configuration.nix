9e1e0bfb9fcb55a314761ff9f92396b2aff4f647
fleaz <mail@felixbreidenstein.de>
2023-12-01 16:58:54 +0100

    Added smithers

---
+++ machines/smithers/configuration.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../roles/all.nix
+      ../../roles/laptop.nix
+
+      # import hardware specific settings
+      <nixos-hardware/framework/13-inch/7040-amd>
+    ];
+
+  # disable the include of TLP because we get the fancy AMD one from nixos-hardwware
+  my.includeTLP = false;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "smithers";
+
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
