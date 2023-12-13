4d0f2431080181f9b8cbeba7526e20346466cbed
fleaz <mail@felixbreidenstein.de>
2023-12-13 12:15:22 +0100

    modules/zsh: Add 'beep' alias to play a small chime

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -14,6 +14,7 @@
       "k" = "kubectl";
       "dl" = "ls -lhtr --color=always ~/Downloads | tail -n 10"; # Show the 10 newest Downloads
       "buzzer" = "ssh -i Nextcloud/Privat/id_door door@door.w17.io buzzer";
+      "beep" = "paplay /usr/share/sounds/freedesktop/stereo/complete.oga"; # play "ding" for long running jobs
     };
     initExtra = ''
       export EDITOR="nvim";
