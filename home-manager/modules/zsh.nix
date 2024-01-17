48e731a4a86783c0a166cb78cf228ae88e731d19
fleaz <mail@felixbreidenstein.de>
2024-01-17 17:51:57 +0100

    home/zsh: Added alias for dig and kubectx

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -13,9 +13,11 @@
     };
     shellAliases = {
       "k" = "kubectl";
+      "kx" = "kubectx";
       "dl" = "ls -lhtr --color=always ~/Downloads | tail -n 10"; # Show the 10 newest Downloads
       "buzzer" = "ssh -i Nextcloud/Privat/id_door door@door.w17.io buzzer";
       "beep" = "paplay /usr/share/sounds/freedesktop/stereo/complete.oga"; # play "ding" for long running jobs
+      "dig" = "dig +short";
     };
     initExtra = ''
       autoload -U colors && colors
