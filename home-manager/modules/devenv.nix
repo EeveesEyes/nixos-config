96d898427ea77c02f878196f6bf02671a054550f
fleaz <mail@felixbreidenstein.de>
2024-06-12 22:03:25 +0200

    home/devenv: Update

--- home-manager/modules/devenv.nix
+++ home-manager/modules/devenv.nix
@@ -1,5 +1,5 @@
 let
-    source = import (fetchTarball "https://github.com/cachix/devenv/archive/v0.6.2.tar.gz");
+    source = import (fetchTarball "https://github.com/cachix/devenv/archive/v1.0.5.tar.gz");
 in
 {
   home.packages = [
