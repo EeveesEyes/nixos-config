effa158913e9bf6c8fcea89785039b9f41f35024
fleaz <mail@felixbreidenstein.de>
2024-07-15 18:47:41 +0200

    Domain switch from w17.io to cccda.de

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -15,7 +15,7 @@
       "k" = "kubectl";
       "kx" = "kubectx";
       "dl" = "ls -lhtr --color=always ~/Downloads | tail -n 10"; # Show the 10 newest Downloads
-      "buzzer" = "ssh -i Nextcloud/Privat/id_door door@door.w17.io buzzer";
+      "buzzer" = "ssh -i Nextcloud/Privat/id_door door@door.cccda.de buzzer";
       "beep" = "paplay /usr/share/sounds/freedesktop/stereo/complete.oga"; # play "ding" for long running jobs
       "dig" = "dig +short";
       "cal" = "ncal -b";
